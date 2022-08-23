8e779a973e3039cdeae9e4bc5b6beaeb454ac6a0
Mark Juzefin <hilarionium@gmail.com>
2022-08-23 10:37:32 +0300

    Student class only

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import com.example.demo.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping //to get something out from our server
+	public List<Student> showInfo(){
+		return List.of(
+				new Student (
+						1L,
+						"Maksym",
+						"maksym@gmail.com",
+						LocalDate.of(2004, Month.MAY, 13),
+						18)
+		);
+	}
+}
