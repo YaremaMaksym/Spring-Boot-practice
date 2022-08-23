7e6a3ecf0bf5f24aa690f36e831d02273beeca2f
max yar <macsym22@gmail.com>
2022-08-23 20:41:49 +0300

    added DataBase layer

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,22 +1,34 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service //like @Component but better
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
 
     @GetMapping //to get something out from our server
     public List<Student> getStudents(){
-        return List.of(
-                new Student (
-                        1L,
-                        "Maksym",
-                        "maksym@gmail.com",
-                        LocalDate.of(2004, Month.MAY, 13),
-                        18)
-        );
+        return studentRepository.findAll();
     }
 }
+
+//new Student (
+//                        1L,
+//                        "Maksym",
+//                        "maksym@gmail.com",
+//                        LocalDate.of(2004, Month.MAY, 13),
+//                        18)
